71833f18810af30d4e5e2ee6bbb8bae9362974e9
PopovDS617 <popovds.bm@gmail.com>
2022-10-04 22:30:47 +0300

    added: marker and site icon

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './App.scss';
 
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import icon from './assets/marker.png';
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
-  shadowUrl: iconShadow,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
